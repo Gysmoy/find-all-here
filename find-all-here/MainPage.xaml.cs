d237f834e688c49252ff7bf9fd5c0e10fec6f2d9
Gysmoy <gamboapalominocarlosmanuel@gmail.com>
2022-03-22 16:36:29 -0500

    NTS

---
+++ find-all-here/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace find_all_here
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ find-all-here/assets/csharp/Database.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace find_all_here.assets.csharp
+{
+    internal class Database
+    {
+
+    }
+}
