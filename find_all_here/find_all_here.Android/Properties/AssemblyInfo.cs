4a26f550bd8c4b7499769c289d160e33a58209b6
Gysmoy <gamboapalominocarlosmanuel@gmail.com>
2022-04-27 15:14:46 -0500

    NTS

--- find_all_here/find_all_here.Android/Properties/AssemblyInfo.cs
+++ find_all_here/find_all_here.Android/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("find_all_here.Android")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Find All Here")]
+[assembly: AssemblyDescription("Aplicación de compra y venta de productos de Segunda Mano")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("find_all_here.Android")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Software Development Perú")]
+[assembly: AssemblyProduct("Find All Here")]
+[assembly: AssemblyCopyright("© Todos los derechos reservados")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
@@ -22,9 +22,11 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2022.1.0.0")]
+[assembly: AssemblyFileVersion("2022.1.0.0")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessNetworkState)]
+[assembly: UsesPermission(Android.Manifest.Permission.Camera)]
