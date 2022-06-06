429ee029f68507caf17c3caf1eb04ff8d30f04b2
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2022-06-06 14:19:27 -0500

    NTS

--- Find All Here/Views/CartView.xaml.cs
+++ Find All Here/Views/CartView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Find_All_Here.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,30 @@ namespace Find_All_Here.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CartView : ContentPage
     {
+        CartViewModel _cartViewModel;
         public CartView()
         {
             InitializeComponent();
+            BindingContext = _cartViewModel = new CartViewModel();
+        }
+        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selectedItem = e.SelectedItem as CartViewModel;
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var selectedItem = e.Item as CartViewModel;
+        }
+
+        async void Btn_Home(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        async void Btn_Pago(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new PaymentView());
         }
     }
 }
